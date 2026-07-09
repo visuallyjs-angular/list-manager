5b0c5d69d43fedc87ec814a331c8fa8c61b5dbee
Simon Porritt <simon@jsplumbtoolkit.com>
2026-07-09 10:42:18 +1000

    fix typings for before connect

--- src/app/model-options.ts
+++ src/app/model-options.ts
@@ -1,12 +1,12 @@
-import { Node, Group} from "@visuallyjs/browser-ui"
+import { Node, Group, Vertex} from "@visuallyjs/browser-ui"
 
 function notInSameGroup(source: Node|Group, target: Node|Group) {
     return source.group == null || target.group == null || source.group !== target.group
 }
 
 const modelOptions = {
-    beforeConnect:(source: Node|Group, target: Node|Group) => {
-        return notInSameGroup(source, target)
+    beforeConnect:(source: Vertex, target: Vertex) => {
+        return notInSameGroup(source as Node|Group, target as Node|Group)
     }
 }
 
